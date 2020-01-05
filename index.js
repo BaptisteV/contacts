cd44d8f58b461eae49dbdf276db9d337b4543f23
BaptisteV <baptiste.viloin.iris@gmail.com>
2020-01-05 17:27:08 +0100

    Basic add a contact in memory

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-'use strict';
+const express = require('express')
+const app = express()
+const fs = require("fs")
 
-var fs = require('fs');
-var path = require('path');
+app.use(express.json())
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
+class Contact {
+  constructor(firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+  format() {
+    return this.firstName + " " + this.lastName;
+  }
+}
 
-  callback(null, result);
-};
+const contacts = [];
+
+// TODO: retrieve from the database
+contacts.push(new Contact("James", "Hetfield"));
+contacts.push(new Contact("Kirk", "Hammet"));
+
+app.get('/', function (req, res) {
+
+  let index = fs.readFileSync(`public/index.html`);
+  // TODO: Use a templating engine
+  formattedContacts = contacts.map(contact => contact.format())
+  index = index.toString().replace("%CONTACTS%", formattedContacts.toString())
+
+  res.send(index)
+})
+
+app.post('/contacts', (req, res) => {
+  console.log("body", req.body)
+  contacts.push(new Contact(req.body.firstName, req.body.lastName));
+  console.log(contacts)
+  res.sendStatus(200)
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000! http://localhost:3000/')
+})
