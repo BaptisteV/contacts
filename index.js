ac33d8f620022bd595e2c16f225011252d648e37
BaptisteV <baptiste.viloin.iris@gmail.com>
2020-01-06 14:45:26 +0100

    Use async await instead of callback

--- index.js
+++ index.js
@@ -24,21 +24,23 @@ class Contact {
   }
 }
 
-app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
   const contacts = []
-  pgClient.query('SELECT "firstName", "lastName" FROM contacts', (err, queryResults) => {
-    console.log(err, queryResults.rows)
 
+  try {
+    const queryResults = await pgClient.query('SELECT "firstName", "lastName" FROM contacts');
     queryResults.rows.forEach((queryResult) => {
       contacts.push(new Contact(queryResult.firstName, queryResult.lastName))
     });
-    const formattedContacts = contacts.map(contact => contact.format())
-    let index = fs.readFileSync(`public/index.html`);
-    // TODO: Use a templating engine
-    index = index.toString().replace("%CONTACTS%", formattedContacts.toString())
-
-    res.send(index)
-  })
+  } catch (err) {
+    console.log(err);
+  }
+  
+  const formattedContacts = contacts.map(contact => contact.format())
+  let index = fs.readFileSync(`public/index.html`);
+  // TODO: Use a templating engine
+  index = index.toString().replace("%CONTACTS%", formattedContacts.toString())
+  res.send(index)
 })
 
 app.post('/contacts', (req, res) => {
